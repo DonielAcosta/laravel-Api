0d41b50963baf56b843ae42c2cf382fa96dcbb4a
Doniel Acosta <donielacosta1995@gmail.com>
2022-09-19 11:46:49 -0400

    creacion de collecion de data de todos los post

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\PostController;
 
-Route::apiResource('v1/posts',PostController::class)->only('show');
+Route::apiResource('v1/posts',PostController::class)->only(['index','show']);
