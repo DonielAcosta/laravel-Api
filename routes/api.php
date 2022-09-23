eef2c6ce8cf050e6b2f85291c4524bc47ba4490f
Doniel Acosta <donielacosta1995@gmail.com>
2022-09-23 11:06:08 -0400

    autrnticacion con api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
+use App\Http\Controllers\Api\LoginController;
 
+Route::apiResource('v1/posts',PostV1::class)->only(['index','show', 'destroy'])->middleware('auth:sanctum');
+Route::apiResource('v2/posts',PostV2::class)->only(['index','show'])->middleware('auth:sanctum');
 
-Route::apiResource('v1/posts',PostV1::class)->only(['index','show', 'destroy']);
-Route::apiResource('v2/posts',PostV2::class)->only(['index','show']);
+Route::post('login',[ LoginController::class,'login' ]);
